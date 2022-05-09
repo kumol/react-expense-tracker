2a6e75e38487e8cad50e1fca9089486ed7608709
kumol <kumol01763@gmail.com>
2022-05-10 00:40:56 +0600

    app dot js history

--- src/App.js
+++ src/App.js
@@ -9,35 +9,27 @@ import Home from './container/Home/Home';
 import History from './container/History/History';
 function App() {
   return (
-    <Home/>
-      // <Router>
-      // <nav>
-      //     <ul>
-      //       <li>
-      //         <Link to="/">Home</Link>
-      //       </li>
-      //       <li>
-      //         <Link to="/monthly">History</Link>
-      //       </li>
-      //       <li>
-      //         <Link to="/expense">expense</Link>
-      //       </li>
-      //     </ul>
-      // </nav>
-      //   <div>
-      //   <Routes>
-      //     <Route path="/expense">
-      //       <Home />
-      //     </Route>
-      //     <Route path="/monthly">
-      //       <History />
-      //     </Route>
-      //     <Route path="/">
-      //       <Home />
-      //     </Route>
-      //   </Routes>
-      //   </div>
-      // </Router>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home" >Home</Link>
+          </li>
+          <li>
+            <Link to="/about" >About</Link>
+          </li>
+          <li>
+            <Link to="/history" >History</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='home' element={<Home/>}>
+        </Route>
+        <Route path='/history' element={<History/>}></Route>
+      </Routes>
+
+    </Router>
   );
 }
 
--- src/container/History/History.js
+++ src/container/History/History.js
@@ -1,12 +1,49 @@
+import axios from 'axios';
+import moment from 'moment';
 import React,{useEffect,useState} from 'react';
+const baseUrl = "https://theexpense.herokuapp.com/";
 const History = (props) => {
 
-    const [history, setHistory] = useState({});
+    const [history, setHistory] = useState([]);
+    const [avg, setAvg] = useState(0);
+    const [total, setTotal] = useState(0);
+
+    const getExpenseHistory = async()=>{
+        let month = moment().format("MM");
+        if(month<10){
+            month = month[1];
+        }
+        let response = await axios.get(`${baseUrl}money/monthly-spent?month=${month}`);
+        
+         let [bodyData] = response.data.body;
+        setHistory(bodyData.dates);
+        setAvg(bodyData.avgCost);
+        setTotal(bodyData.total);
+    }
+    useEffect(()=>{
+        getExpenseHistory();
+    }, [])
     return (
-        <div>
+        <div className='content-body'>
             <div>Header of History</div>
             <div>
-                
+                <ul style={{listStyle: "none"}}>
+                    <li>
+                        <div className='list'>
+                            <div className='amount'>Average: {avg.toFixed(2)}</div>
+                            <div className='details'> per day of {history.length} <span style={{float: "right"}}> Total : {total}</span></div>
+                        </div>
+                    </li>
+                    {
+                        history.length>0 ? history.map(h=>{
+                            return (<li className='main_list'><div className='list'>
+                            <div className='amount'>{h.money}</div>
+                            <div className='details'> <span style={{float: "right"}}>{moment(h.date).format("LL")}</span></div>
+                        </div></li>)
+                        }) : <p>hello</p>
+                    }
+                    
+                </ul>
             </div>
         </div>
     );
